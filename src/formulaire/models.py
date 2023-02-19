aa8efebb68e8019124f2d6030a90270e561f6fde
trouvpadenom <bricetrp38@icloud.com>
2023-02-19 03:35:07 +0100

    enft pas de formulaire

--- src/formulaire/models.py
+++ src/formulaire/models.py
@@ -1,11 +1,3 @@
-# Create your models here.
-from .models import Utilisateur
-from django import forms
 from django.db import models
 
-
-
-class Utilisateur(models.Model):
-    pseudo = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
-    mdp = models.CharField(max_length=50)
+# Create your models here.
--- src/formulaire/views.py
+++ src/formulaire/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from .forms import InscriptionForm
+
 
 # Create your views here.
 def index(request):
     return render(request, "formulaire/index.html")
-
-class InscriptionView(CreateView):
-    template_name = 'inscription.html'
-    form_class = InscriptionForm
-    success_url = '/merci/'
