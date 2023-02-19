ac47d1f693f7b060178f1520a150abdae444719d
J-IFT <juliette.infanti@gmail.com>
2023-02-19 03:59:14 +0100

    formulaire de mes couilles

--- src/formulaire/urls.py
+++ src/formulaire/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index
+from .views import connexion
 
 urlpatterns = [
-    path('', index, name="formulaire-index")
+    path('', index, name="formulaire-index"),
+    path('connexion', connexion, name="formulaire-connexion"),
 ]
