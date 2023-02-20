ade59536d73b2770127a26e5b847fb153ad5af48
aurock <jeremy.dussol@epsi.fr>
2023-02-20 07:26:38 +0100

    🚧

--- src/formulaire/views.py
+++ src/formulaire/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
-    return render(request, "formulaire/index.html")
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("formulaire/connexion.html")
+        messages.error(request, form.errors)
+    form = NewUserForm()
+    return render (request, "formulaire/index.html", {"form":form})
 
 def connexion(request):
     return render(request, "formulaire/connexion.html")
